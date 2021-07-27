f717e6d4cd78d1a7aa896baab3d7cdb217fabf83
telmocode <telmogferreira@gmail.com>
2021-07-27 14:47:09 -0300

    finalizado

--- src/main/java/com/telmocode/crm/model/Clientes.java
+++ src/main/java/com/telmocode/crm/model/Clientes.java
@@ -1,7 +1,5 @@
 package com.telmocode.crm.model;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/telmocode/crm/repository/ClienteRepository.java
+++ src/main/java/com/telmocode/crm/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.telmocode.crm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.telmocode.crm.model.Clientes;

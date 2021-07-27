cf40de2bacbb836dbd45726753674489a926d2b3
telmocode <telmogferreira@gmail.com>
2021-07-27 15:43:57 -0300

    atualização pos final

--- src/main/java/com/telmocode/crm/controller/ClienteControlle.java
+++ src/main/java/com/telmocode/crm/controller/ClienteControlle.java
@@ -3,10 +3,12 @@ package com.telmocode.crm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telmocode.crm.model.Clientes;
@@ -26,6 +28,7 @@ public class ClienteControlle {
 	}
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Clientes adicionar(@RequestBody Clientes cliente) {
 		
 		return clienteRepository.save(cliente);
